2ede0d6af29994ec6bd3149ebac2081ff57ce515
Slava Vorotov <madbadrat301@yandex.ru>
2025-03-09 19:48:19 +0700

    Поправил выравнивание в карточках на странице истории заказов

--- components/OrderInformationRow.tsx
+++ components/OrderInformationRow.tsx
@@ -9,10 +9,10 @@ type Props = {
 export function OrderInformationRow({ label, text }: Props) {
     return (
         <View style={styles.orderInformationRow}>
-            <View>
+            <View style={styles.orderInformationRowTextContainer}>
                 <Text style={styles.orderInformationRowText}>{label}</Text>
             </View>
-            <View>
+            <View style={styles.orderInformationRowTextContainer}>
                 <Text style={styles.orderInformationRowText}>{text}</Text>
             </View>
         </View>
@@ -23,11 +23,18 @@ const styles = StyleSheet.create({
     orderInformationRow: {
         flexDirection: 'row',
         justifyContent: 'space-around',
+        alignItems: 'center',
         width: '100%',
         marginBottom: 16
     },
     orderInformationRowText: {
         color: Colors.white,
-        fontSize: 22
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    orderInformationRowTextContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '50%'
     }
 })
